bd29a5d348e23fe8e3f058eb732ccbdba79c6188
Misa102 <hoquynhnhu243@gmail.com>
2024-01-19 10:41:03 +0100

    update client

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -1,8 +1,12 @@
+
 const { Client, Intents } = require('discord.js');
+const mongoose = require('mongoose');
+const Citation = require('./citationModel');
+
+// Create a new Discord client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const mongoose = require('mongoose');
-
+// Connect to MongoDB
 mongoose.connect('mongodb+srv://admin:webm1@cluster0.7mbewn7.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,111 +17,72 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
-const mongoose = require('mongoose');
 
-const citationSchema = new mongoose.Schema({
-  content: String,
-  author: String,
-  date: String,
-});
-
-const Citation = mongoose.model('Citation', citationSchema);
-
-module.exports = Citation;
-const Citation = require('./citationModel'); 
-
-
-
-// Arrays to store citations and favorites
-
+// Event when the bot is ready
 client.on('ready', () => {
-    console.log(`${client.user.tag} has logged in!`);
-
-    // Set the bot's activity
-    client.user.setActivity('Managing Citations', { type: 'WATCHING' });
+  console.log(`${client.user.tag} has logged in!`);
+  client.user.setActivity('Managing Citations', { type: 'WATCHING' });
 });
-client.on('messageCreate', (message) => {
-    console.log('Message received:', message.content);
 
-    if (message.author.bot) return; // Ignore messages from other bots
+// Event when a message is received
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
 
-    //the message content
-    const args = message.content.split(' ');
-    const command = args[0].toLowerCase();
-    console.log('Command:', command);
-    console.log('Args:', args.slice(1));
+  const args = message.content.split(' ');
+  const command = args[0].toLowerCase();
 
-    if (message.content.toLowerCase() === 'ping') {
-        message.reply('Pong!');
-    }
-
-    if (message.content.toLowerCase() === 'quoi?') {
-        message.reply('quoicoubah!');
-    }
-
-    if (command === '/trd-add-citation') {
-        // Command to add a new citation
+  if (command === '/trd-add-citation') {
         const content = args.slice(1).join(' ');
+    
         if (content) {
-            const newCitation = {
+            const newCitation = new Citation({
                 content: content,
                 author: message.author.username,
                 date: new Date().toLocaleDateString(),
-            };
-            citations.push(newCitation);
-            message.reply('Added a new citation!').then(() => {
-                console.log('Reply sent successfully');
-            })
-                .catch((error) => {
-                    console.error('Error sending reply:', error);
-                });
+            });
+    
+            newCitation.save((err) => {
+                if (err) {
+                    console.error('Error saving citation:', err);
+                    message.reply('Error adding citation.');
+                } else {
+                    message.reply('Added a new citation!');
+                }
+            });
         } else {
-            message.reply('Please provide the content for the citation.')
-                .then(() => {
-                    console.log('Reply sent successfully');
-                })
-                .catch((error) => {
-                    console.error('Error sending reply:', error);
-                });
+            message.reply('Please provide the content for the citation.');
         }
+    
+    
     } else if (command === '/trd-list-citations') {
-        // Command to list all citations
-        if (citations.length > 0) {
+        try {
+          const citations = await Citation.find();
+          if (citations.length > 0) {
             const citationList = citations.map((citation) => `- ${citation.content} (By: ${citation.author}, Date: ${citation.date})`);
-            message.reply(`List of Citations:\n${citationList.join('\n')}`).then(() => {
-                console.log('Reply sent successfully');
-            })
-                .catch((error) => {
-                    console.error('Error sending reply:', error);
-                });
-        } else {
-            message.reply('No citations available.').then(() => {
-                console.log('Reply sent successfully');
-            })
-                .catch((error) => {
-                    console.error('Error sending reply:', error);
-                });
+            message.reply(`List of Citations:\n${citationList.join('\n')}`);
+          } else {
+            message.reply('No citations available.');
+          }
+        } catch (error) {
+          console.error('Error fetching citations:', error);
+          message.reply('Error fetching citations.');
         }
-    } else if (command === '/trd-list-favorites') {
-        // Command to list favorite citations
-        if (favorites.length > 0) {
+      } else if (command === '/trd-list-favorites') {
+        try {
+          
+          const favorites = await Citation.find({  });
+          if (favorites.length > 0) {
             const favoriteList = favorites.map((favorite) => `- ${favorite.content} (By: ${favorite.author}, Date: ${favorite.date})`);
-            message.reply(`List of Favorite Citations:\n${favoriteList.join('\n')}`).then(() => {
-                console.log('Reply sent successfully');
-            })
-                .catch((error) => {
-                    console.error('Error sending reply:', error);
-                });
-        } else {
-            message.reply('No favorite citations available.') .then(() => {
-                console.log('Reply sent successfully');
-            })
-                .catch((error) => {
-                    console.error('Error sending reply:', error);
-                });
+            message.reply(`List of Favorite Citations:\n${favoriteList.join('\n')}`);
+          } else {
+            message.reply('No favorite citations available.');
+          }
+        } catch (error) {
+          console.error('Error fetching favorite citations:', error);
+          message.reply('Error fetching favorite citations.');
         }
-    }
-});
+      }
+    });
 
 
 
